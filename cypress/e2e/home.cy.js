1f43be4f4bc8cc5b873350c2ccd8ee3215166d94
webertamas <webertamasss@gmail.com>
2023-09-16 19:43:52 +0200

    home

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,38 @@
+describe('home page', () => {
+  beforeEach(() => {
+    cy.visit('http://www.upscale.hu')
+  })
+
+  context('Cookies', () => {
+  it('Accept cookies', () => {
+    cy.get('.done-gdpr-button-allow > .done-gdpr-alert-box-button-middle').as('acceptCookieButton')
+    
+    cy.get('@acceptCookieButton').click()
+    cy.get('@acceptCookieButton').should('not.be.visible')
+    debugger
+  })
+
+  it('Edit cookies settings', () => {
+    cy.get('.done-gdpr-toggle-display').click()
+    cy.get('#done-gdpr-popup-body-right > h2').contains('Adatvédelmi Preferencia Beállítások')
+    cy.get('#done-gdpr-save-button').click()
+    cy.get('#done-gdpr-popup-body-right > h2').should('not.be.visible')
+  })
+
+  it('Cookie policies', () => {
+    cy.get('.done-gdpr-alert-box-body > p > a').click()
+    cy.location('pathname').should('equal', '/adatkezelesi-tajekoztato/')
+
+  
+  })
+
+
+
+})
+
+  context('something', () => {
+    it('Accept cookies', () => {
+      cy.get('.done-gdpr-button-allow > .done-gdpr-alert-box-button-middle')
+    })
+  })
+})
